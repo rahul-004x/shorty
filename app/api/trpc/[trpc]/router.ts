5d335cdca7963cb9074bfe6426e541aea79cb88f
rahul <rahulxyz123u@gmail.com>
2025-07-18 13:38:38 +0530

    feat: tRPC edge handler

---
+++ app/api/trpc/[trpc]/router.ts
@@ -0,0 +1,5 @@
+import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { appRouter } from '@/server/routers/_app';
+import { createContext } from '@/server/context';
+
+export default createNextApiHandler({ router: appRouter, createContext });
